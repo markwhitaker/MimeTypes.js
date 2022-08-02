74219000ad56b4af5fbcee87d0ec1a4ecfc4070f
Mark Whitaker <markw@bitrot.net>
2022-08-02 19:23:30 +0100

    Add MIME types and unit tests

--- src/mime-types.js
+++ src/mime-types.js
@@ -1,10 +1,98 @@
 "use strict";
 
 const MimeTypes = Object.freeze({
-    Text: Object.freeze({
-        Html: "text/html",
-        Plain: "text/plain"
-    })
+    Application: (prefix => Object.freeze({
+        AtomXml: prefix + "atom+xml",
+        AtomcatXml: prefix + "atomcat+xml",
+        Ecmascript: prefix + "ecmascript",
+        JavaArchive: prefix + "java-archive",
+        Javascript: prefix + "javascript",
+        Json: prefix + "json",
+        Mp4: prefix + "mp4",
+        OctetStream: prefix + "octet-stream",
+        Pdf: prefix + "pdf",
+        Pkcs10: prefix + "pkcs10",
+        Pkcs7Mime: prefix + "pkcs7-mime",
+        Pkcs7Signature: prefix + "pkcs7-signature",
+        Pkcs8: prefix + "pkcs8",
+        Postscript: prefix + "postscript",
+        RdfXml: prefix + "rdf+xml",
+        RssXml: prefix + "rss+xml",
+        Rtf: prefix + "rtf",
+        SmilXml: prefix + "smil+xml",
+        XFontOff: prefix + "x-font-off",
+        XPkcs12: prefix + "x-pkcs12",
+        XShockwaveFlash: prefix + "x-shockwave-flash",
+        XSilverlightApp: prefix + "x-silverlight-app",
+        XhtmlXml: prefix + "xhtml+xml",
+        Xml: prefix + "xml",
+        XmlDtd: prefix + "xml-dtd",
+        XsltXml: prefix + "xslt+xml",
+        XWwwFormUrlencoded: prefix + "x-www-form-urlencoded",
+        Zip: prefix + "zip"
+    }))("application/"),
+
+    Audio: (prefix => Object.freeze({
+        Midi: prefix + "midi",
+        Mp4: prefix + "mp4",
+        Mpeg: prefix + "mpeg",
+        Ogg: prefix + "ogg",
+        Wav: prefix + "wav",
+        Webm: prefix + "webm",
+        XAac: prefix + "x-aac",
+        XAiff: prefix + "x-aiff",
+        XMpegurl: prefix + "x-mpegurl",
+        XMsWma: prefix + "x-ms-wma",
+        XWav: prefix + "x-wav"
+    }))("audio/"),
+
+    Font: (prefix => Object.freeze({
+        Collection: prefix + "collection",
+        Otf: prefix + "otf",
+        Sfnt: prefix + "sfnt",
+        Ttf: prefix + "ttf",
+        Woff: prefix + "woff",
+        Woff2: prefix + "woff2"
+    }))("font/"),
+
+    Image: (prefix => Object.freeze({
+        Bmp: prefix + "bmp",
+        Gif: prefix + "gif",
+        Jpeg: prefix + "jpeg",
+        Png: prefix + "png",
+        SvgXml: prefix + "svg+xml",
+        Tiff: prefix + "tiff",
+        Webp: prefix + "webp"
+    }))("image/"),
+
+    Multipart: (prefix => Object.freeze({
+        Byteranges: prefix + "byteranges",
+        Encrypted: prefix + "encrypted",
+        FormData: prefix + "form-data",
+        Related: prefix + "related"
+    }))("multipart/"),
+
+    Text: (prefix => Object.freeze({
+        Css: prefix + "css",
+        Csv: prefix + "csv",
+        Html: prefix + "html",
+        Javascript: prefix + "javascript",
+        Plain: prefix + "plain",
+        Richtext: prefix + "richtext",
+        Sgml: prefix + "sgml",
+        Xml: prefix + "xml",
+        Yaml: prefix + "yaml"
+    }))("text/"),
+
+    Video: (prefix => Object.freeze({
+        H264: prefix + "h264",
+        Mp4: prefix + "mp4",
+        Mpeg: prefix + "mpeg",
+        Ogg: prefix + "ogg",
+        Quicktime: prefix + "quicktime",
+        ThreeGpp: prefix + "3gpp",
+        Webm: prefix + "webm"
+    }))("video/")
 });
 
 export default MimeTypes;
--- test/mime-types-tests.js
+++ test/mime-types-tests.js
@@ -4,8 +4,84 @@ import {MimeTypes} from "../index.js";
 
 QUnit.module("MimeTypes");
 
+assertEqual("application/atom+xml", MimeTypes.Application.AtomXml);
+assertEqual("application/atomcat+xml", MimeTypes.Application.AtomcatXml);
+assertEqual("application/ecmascript", MimeTypes.Application.Ecmascript);
+assertEqual("application/java-archive", MimeTypes.Application.JavaArchive);
+assertEqual("application/javascript", MimeTypes.Application.Javascript);
+assertEqual("application/json", MimeTypes.Application.Json);
+assertEqual("application/mp4", MimeTypes.Application.Mp4);
+assertEqual("application/octet-stream", MimeTypes.Application.OctetStream);
+assertEqual("application/pdf", MimeTypes.Application.Pdf);
+assertEqual("application/pkcs10", MimeTypes.Application.Pkcs10);
+assertEqual("application/pkcs7-mime", MimeTypes.Application.Pkcs7Mime);
+assertEqual("application/pkcs7-signature", MimeTypes.Application.Pkcs7Signature);
+assertEqual("application/pkcs8", MimeTypes.Application.Pkcs8);
+assertEqual("application/postscript", MimeTypes.Application.Postscript);
+assertEqual("application/rdf+xml", MimeTypes.Application.RdfXml);
+assertEqual("application/rss+xml", MimeTypes.Application.RssXml);
+assertEqual("application/rtf", MimeTypes.Application.Rtf);
+assertEqual("application/smil+xml", MimeTypes.Application.SmilXml);
+assertEqual("application/x-font-off", MimeTypes.Application.XFontOff);
+assertEqual("application/x-pkcs12", MimeTypes.Application.XPkcs12);
+assertEqual("application/x-shockwave-flash", MimeTypes.Application.XShockwaveFlash);
+assertEqual("application/x-silverlight-app", MimeTypes.Application.XSilverlightApp);
+assertEqual("application/xhtml+xml", MimeTypes.Application.XhtmlXml);
+assertEqual("application/xml", MimeTypes.Application.Xml);
+assertEqual("application/xml-dtd", MimeTypes.Application.XmlDtd);
+assertEqual("application/xslt+xml", MimeTypes.Application.XsltXml);
+assertEqual("application/x-www-form-urlencoded", MimeTypes.Application.XWwwFormUrlencoded);
+assertEqual("application/zip", MimeTypes.Application.Zip);
+
+assertEqual("audio/midi", MimeTypes.Audio.Midi);
+assertEqual("audio/mp4", MimeTypes.Audio.Mp4);
+assertEqual("audio/mpeg", MimeTypes.Audio.Mpeg);
+assertEqual("audio/ogg", MimeTypes.Audio.Ogg);
+assertEqual("audio/wav", MimeTypes.Audio.Wav);
+assertEqual("audio/webm", MimeTypes.Audio.Webm);
+assertEqual("audio/x-aac", MimeTypes.Audio.XAac);
+assertEqual("audio/x-aiff", MimeTypes.Audio.XAiff);
+assertEqual("audio/x-mpegurl", MimeTypes.Audio.XMpegurl);
+assertEqual("audio/x-ms-wma", MimeTypes.Audio.XMsWma);
+assertEqual("audio/x-wav", MimeTypes.Audio.XWav);
+
+assertEqual("font/collection", MimeTypes.Font.Collection);
+assertEqual("font/otf", MimeTypes.Font.Otf);
+assertEqual("font/sfnt", MimeTypes.Font.Sfnt);
+assertEqual("font/ttf", MimeTypes.Font.Ttf);
+assertEqual("font/woff", MimeTypes.Font.Woff);
+assertEqual("font/woff2", MimeTypes.Font.Woff2);
+
+assertEqual("image/bmp", MimeTypes.Image.Bmp);
+assertEqual("image/gif", MimeTypes.Image.Gif);
+assertEqual("image/jpeg", MimeTypes.Image.Jpeg);
+assertEqual("image/png", MimeTypes.Image.Png);
+assertEqual("image/svg+xml", MimeTypes.Image.SvgXml);
+assertEqual("image/tiff", MimeTypes.Image.Tiff);
+assertEqual("image/webp", MimeTypes.Image.Webp);
+
+assertEqual("multipart/byteranges", MimeTypes.Multipart.Byteranges);
+assertEqual("multipart/encrypted", MimeTypes.Multipart.Encrypted);
+assertEqual("multipart/form-data", MimeTypes.Multipart.FormData);
+assertEqual("multipart/related", MimeTypes.Multipart.Related);
+
+assertEqual("text/css", MimeTypes.Text.Css);
+assertEqual("text/csv", MimeTypes.Text.Csv);
 assertEqual("text/html", MimeTypes.Text.Html);
+assertEqual("text/javascript", MimeTypes.Text.Javascript);
 assertEqual("text/plain", MimeTypes.Text.Plain);
+assertEqual("text/richtext", MimeTypes.Text.Richtext);
+assertEqual("text/sgml", MimeTypes.Text.Sgml);
+assertEqual("text/xml", MimeTypes.Text.Xml);
+assertEqual("text/yaml", MimeTypes.Text.Yaml);
+
+assertEqual("video/h264", MimeTypes.Video.H264);
+assertEqual("video/mp4", MimeTypes.Video.Mp4);
+assertEqual("video/mpeg", MimeTypes.Video.Mpeg);
+assertEqual("video/ogg", MimeTypes.Video.Ogg);
+assertEqual("video/quicktime", MimeTypes.Video.Quicktime);
+assertEqual("video/3gpp", MimeTypes.Video.ThreeGpp);
+assertEqual("video/webm", MimeTypes.Video.Webm);
 
 function assertEqual(expected, actual) {
     QUnit.test("Test " + expected, (assert) => assert.equal(expected, actual));
